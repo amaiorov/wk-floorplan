02d7c5aabbd9d704067c613f46a71622cd7db256
joeonmars <joeonmars@gmail.com>
2016-02-05 16:33:13 -0500

    update;

--- source/js/models/employee.js
+++ source/js/models/employee.js
@@ -19,7 +19,7 @@ var Employee = function( props ) {
 	this.cellPhone = props[ 'CellPhone#s' ];
 	this.initials = this.getInitials();
 	this.fullName = this.getFullName();
-	this.type = this.getType( props );
+	this.type = props[ 'Special' ].toLowerCase() || 'employee';
 	if ( this.type === 'employee' || this.type === 'intern' || this.type === 'freelance' ) {
 		this.photoURL = '/service.php?fullName=' + sanitize( this.firstName ) + '.' + sanitize( this.lastName.split( '/' )[ 0 ] );
 	} else {
@@ -35,23 +35,6 @@ var Employee = function( props ) {
 
 	this._observer = new ObjectObserver( this );
 	this._observer.open( this._$onObserved );
-}
-
-
-Employee.prototype.getType = function( props ) {
-
-	switch ( true ) {
-		case ( props[ 'Printer' ].length > 0 ):
-			return 'printer';
-		case ( props[ 'Room' ].length > 0 ):
-			return 'room';
-		case ( props[ 'Freelance' ].length > 0 ):
-			return 'freelance';
-		case ( props[ 'Intern' ].length > 0 ):
-			return 'intern';
-		default:
-			return 'employee';
-	}
 }
 
 
